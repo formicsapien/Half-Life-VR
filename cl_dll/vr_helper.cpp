bbb057805e03ee76eaffb4b76679f74a14ff7c42
Devin <formic.sapien@gmail.com>
2017-10-02 18:52:41 -0700

    re-added touchpad movement

--- cl_dll/vr_helper.cpp
+++ cl_dll/vr_helper.cpp
@@ -242,7 +242,117 @@ void VRHelper::PollEvents()
 		}
 	}
 }
-
+void VRHelper::HandleButtonPress(vr::TrackedDeviceIndex_t trackedDeviceIndex, uint32_t button, bool downOrUp)		
+{		
+vr::ETrackedControllerRole controllerRole = vrSystem->GetControllerRoleForTrackedDeviceIndex(trackedDeviceIndex);		
+if (controllerRole == vr::ETrackedControllerRole::TrackedControllerRole_Invalid)		
+{		
+	return;		
+}		
+	
+if (controllerRole == vr::ETrackedControllerRole::TrackedControllerRole_RightHand)		
+{		
+	switch (button)		
+	{		
+	case vr::EVRButtonId::k_EButton_Grip:		
+		{		
+			downOrUp ? ClientCmd("+reload") : ClientCmd("-reload");		
+		}		
+		break;		
+	case vr::EVRButtonId::k_EButton_ApplicationMenu:		
+		{		
+			downOrUp ? ClientCmd("+attack2") : ClientCmd("-attack2");		
+		}		
+		break;		
+	case vr::EVRButtonId::k_EButton_SteamVR_Trigger:		
+		{		
+			downOrUp ? ClientCmd("+attack") : ClientCmd("-attack");		
+		}		
+		break;		
+	case vr::EVRButtonId::k_EButton_SteamVR_Touchpad:		
+		{		
+			vr::VRControllerState_t controllerState;		
+			vrSystem->GetControllerState(trackedDeviceIndex, &controllerState, sizeof(controllerState));		
+			vr::VRControllerAxis_t touchPadAxis = controllerState.rAxis[vr::EVRButtonId::k_EButton_SteamVR_Touchpad - vr::EVRButtonId::k_EButton_Axis0];		
+	
+			if (touchPadAxis.y > 0.5f && !downOrUp)		
+			{		
+				gHUD.m_Ammo.UserCmd_NextWeapon();		
+				gHUD.m_iKeyBits |= IN_ATTACK;		
+				gHUD.m_Ammo.Think();		
+			}		
+			else if (touchPadAxis.y < -0.5f && !downOrUp)		
+			{		
+				gHUD.m_Ammo.UserCmd_PrevWeapon();		
+				gHUD.m_iKeyBits |= IN_ATTACK;		
+				gHUD.m_Ammo.Think();		
+			}		
+		}		
+		break;		
+	}		
+}		
+else if (controllerRole == vr::ETrackedControllerRole::TrackedControllerRole_LeftHand)		
+{		
+	switch (button)		
+	{		
+	case vr::EVRButtonId::k_EButton_ApplicationMenu:		
+		{		
+			ClientCmd("escape");		
+		}		
+		break;		
+	case vr::EVRButtonId::k_EButton_SteamVR_Trigger:		
+		{		
+			downOrUp ? ClientCmd("+jump") : ClientCmd("-jump");		
+		}		
+		break;		
+	case vr::EVRButtonId::k_EButton_SteamVR_Touchpad:		
+		{		
+			vr::VRControllerState_t controllerState;		
+			vrSystem->GetControllerState(trackedDeviceIndex, &controllerState, sizeof(controllerState));		
+			vr::VRControllerAxis_t touchPadAxis = controllerState.rAxis[vr::EVRButtonId::k_EButton_SteamVR_Touchpad - vr::EVRButtonId::k_EButton_Axis0];		
+	
+			// TODO: Move in direction controller is pointing, not direction player is looking!		
+	
+			if (touchPadAxis.x < -0.5f && downOrUp)		
+			{		
+				ClientCmd("+moveleft");		
+			}		
+			else		
+			{		
+				ClientCmd("-moveleft");		
+			}		
+	
+			if (touchPadAxis.x > 0.5f && downOrUp)		
+			{		
+				ClientCmd("+moveright");		
+			}		
+			else		
+			{		
+				ClientCmd("-moveright");		
+			}		
+	
+			if (touchPadAxis.y > 0.5f && downOrUp)		
+			{		
+				ClientCmd("+forward");		
+			}		
+			else		
+			{		
+				ClientCmd("-forward");		
+			}		
+					
+			if (touchPadAxis.y < -0.5f && downOrUp)		
+			{		
+				ClientCmd("+back");		
+			}		
+			else		
+			{		
+				ClientCmd("-back");		
+			}		
+		}		
+		break;		
+	}		
+}		
+}
 bool VRHelper::UpdatePositions(struct ref_params_s* pparams)
 {
 	if (vrSystem != nullptr && vrCompositor != nullptr)
