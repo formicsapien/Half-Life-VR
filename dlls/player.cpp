fe7bbc025c00f4191be03b1598200ae2c339b5b7
Devin <formic.sapien@gmail.com>
2017-10-02 17:59:50 -0700

    restoring jump

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -1468,7 +1468,52 @@ void CBasePlayer::PlayerUse ( void )
 
 void CBasePlayer::Jump()
 {
-	// Disable jump in VR
+Vector		vecWallCheckDir;// direction we're tracing a line to find a wall when walljumping		 +	// Disable jump in VR
+Vector		vecAdjustedVelocity;		
+Vector		vecSpot;		
+TraceResult	tr;		
+ 		
+if (FBitSet(pev->flags, FL_WATERJUMP))		
+	return;		
+ 		
+if (pev->waterlevel >= 2)		
+{		
+ 	return;		
+}		
+ 	
+// jump velocity is sqrt( height * gravity * 2)		
+ 	
+// If this isn't the first frame pressing the jump button, break out.		
+if ( !FBitSet( m_afButtonPressed, IN_JUMP ) )		
+ 	return;         // don't pogo stick		
+ 	
+if ( !(pev->flags & FL_ONGROUND) || !pev->groundentity )		
+{		
+ 	return;		
+}		
+ 	
+// many features in this function use v_forward, so makevectors now.		
+UTIL_MakeVectors (pev->angles);		
+ 	
+// ClearBits(pev->flags, FL_ONGROUND);		// don't stairwalk		
+ 		
+SetAnimation( PLAYER_JUMP );		
+ 	
+if ( m_fLongJump &&		
+	(pev->button & IN_DUCK) &&		
+ 	( pev->flDuckTime > 0 ) &&		
+ 	pev->velocity.Length() > 50 )		
+{		
+ 	SetAnimation( PLAYER_SUPERJUMP );		
+}		
+ 	
+// If you're standing on a conveyor, add it's velocity to yours (for momentum)		
+entvars_t *pevGround = VARS(pev->groundentity);		
+if ( pevGround && (pevGround->flags & FL_CONVEYOR) )		
+{		
+ 	pev->velocity = pev->velocity + pev->basevelocity;		
+}		
+
 }
 
 
